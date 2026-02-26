04580068bbe8d1a8892feec557b5ebbaa45766f8
Ken Murchison <murch@fastmail.com>
2026-02-26 13:15:21 -0500

    Remove legacy JMAP Contacts code and tests (old API using vparse.c)
    Also rename libicalvcard-based foo_x() functions to foo()

--- imap/vcard_support.h
+++ imap/vcard_support.h
@@ -9,39 +9,24 @@
 
 #include <libical/vcard.h>
 
-#include "vparse.h"
-
 #include "mailbox.h"
 #include "message_guid.h"
 #include "util.h"
 
-extern struct vparse_card *vcard_parse_string(const char *str);
-extern struct vparse_card *vcard_parse_buf(const struct buf *buf);
-extern struct buf *vcard_as_buf(struct vparse_card *vcard);
+#define VCARD_APPLE_LABEL_PROPERTY "X-ABLabel"
+#define VCARD_APPLE_ABADR_PROPERTY "X-ABADR"
 
-extern struct vparse_card *record_to_vcard(struct mailbox *mailbox,
-                                           const struct index_record *record);
+extern vcardcomponent *vcard_parse_string(const char *str);
+extern vcardcomponent *vcard_parse_buf(const struct buf *buf);
+extern struct buf *vcard_as_buf(vcardcomponent *vcard);
 
-extern size_t vcard_prop_decode_value(struct vparse_entry *prop,
+extern vcardcomponent *record_to_vcard(struct mailbox *mailbox,
+                                       const struct index_record *record);
+
+extern size_t vcard_prop_decode_value(vcardproperty *prop,
                                       struct buf *value,
                                       char **content_type,
                                       struct message_guid *guid);
-
-extern void vcard_to_v3(struct vparse_card *vcard);
-extern void vcard_to_v4(struct vparse_card *vcard);
-
-
-extern vcardcomponent *vcard_parse_string_x(const char *str);
-extern vcardcomponent *vcard_parse_buf_x(const struct buf *buf);
-extern struct buf *vcard_as_buf_x(vcardcomponent *vcard);
-
-extern vcardcomponent *record_to_vcard_x(struct mailbox *mailbox,
-                                         const struct index_record *record);
-
-extern size_t vcard_prop_decode_value_x(vcardproperty *prop,
-                                        struct buf *value,
-                                        char **content_type,
-                                        struct message_guid *guid);
 
 extern const char *vcardproperty_get_xparam_value(vcardproperty *prop,
                                                   const char *name);
